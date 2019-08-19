15571ad5bb73e508bd4e206515dd498564ff5022
Lorena Campoverde <lscampov@espol.edu.ec>
2019-08-18 23:31:18 -0500

    clase Admin

--- app/src/main/java/com/lscampov/firebase/MainActivity.java
+++ app/src/main/java/com/lscampov/firebase/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -13,10 +15,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txt1,txt2,txt3,txt4;
+    EditText txt1,txt2,txt3,txt4,txt5,txt6,txt7,txt8;
+    Spinner sp2;
     Button btn;
     DatabaseReference reff;
-    Member member;
+    Administrador admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +27,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this, "Conexion Establecida",Toast.LENGTH_LONG).show();
 
-        txt1= (EditText)findViewById(R.id.editText3);
-        txt2= (EditText)findViewById(R.id.editText4);
+        txt1= (EditText)findViewById(R.id.editText);
+        txt2= (EditText)findViewById(R.id.editText2);
         txt3= (EditText)findViewById(R.id.editText5);
         txt4= (EditText)findViewById(R.id.editText6);
+        txt5= (EditText)findViewById(R.id.editText3);
+        txt6= (EditText)findViewById(R.id.editText4);
+        txt7= (EditText)findViewById(R.id.editText7);
+        txt8= (EditText)findViewById(R.id.editText8);
+        sp2= (Spinner)findViewById(R.id.spinner2);
+
+        String [] perfiles= {"Administrador","Residente"};
+        ArrayAdapter<String> adapter1= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, perfiles);
+        sp2.setAdapter(adapter1);
+
         btn= (Button)findViewById(R.id.button);
-        member= new Member();
-        reff= FirebaseDatabase.getInstance().getReference().child("Member");
+        admin= new Administrador();
+        reff= FirebaseDatabase.getInstance().getReference().child("Perfil");
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int edad= Integer.parseInt(txt2.getText().toString().trim());
-                Float peso= Float.parseFloat(txt4.getText().toString().trim());
-                Long telf= Long.parseLong(txt3.getText().toString().trim());
+                String seleccion= sp2.getSelectedItem().toString();
+                if (seleccion.equals("Administrador")){
+                    admin.setNombre1(txt1.getText().toString().trim());
+                    admin.setNombre2(txt2.getText().toString().trim());
+                    admin.setApellido1(txt3.getText().toString().trim());
+                    admin.setApellido2(txt4.getText().toString().trim());
+                    admin.setCedula(txt5.getText().toString().trim());
+                    admin.setUser(txt6.getText().toString().trim());
+                    admin.setPassword(txt7.getText().toString().trim());
+                    admin.setEmail(txt8.getText().toString().trim());
 
-                member.setName(txt1.getText().toString().trim());
-                member.setAge(edad);
-                member.setHt(peso);
-                member.setPhone(telf);
-                reff.child("member1").setValue(member);
-                Toast.makeText(MainActivity.this,"data success",Toast.LENGTH_LONG).show();
+                    reff.child("Administrador").child(admin.getCedula()).setValue(admin);
+                    Toast.makeText(MainActivity.this,"data success",Toast.LENGTH_LONG).show();
+            }
             }
         });
 
--- app/src/main/java/com/lscampov/firebase/Member.java
+++ app/src/main/java/com/lscampov/firebase/Member.java
@@ -2,9 +2,9 @@ package com.lscampov.firebase;
 
 public class Member {
     private String name;
-    private Integer age;
-    private Long phone;
-    private Float ht;
+    private double age;
+    private double phone;
+    private double ht;
 
     public Member() {
 
@@ -18,27 +18,27 @@ public class Member {
         this.name = name;
     }
 
-    public Integer getAge() {
+    public double getAge() {
         return age;
     }
 
-    public void setAge(Integer age) {
+    public void setAge(double age) {
         this.age = age;
     }
 
-    public Long getPhone() {
+    public double getPhone() {
         return phone;
     }
 
-    public void setPhone(Long phone) {
+    public void setPhone(double phone) {
         this.phone = phone;
     }
 
-    public Float getHt() {
+    public double getHt() {
         return ht;
     }
 
-    public void setHt(Float ht) {
+    public void setHt(double ht) {
         this.ht = ht;
     }
 }
